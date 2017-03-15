d14a4eed9882c0c0135d8640eba1742ce31d73d1
fullmetalfelix <vampirer@tiscalinet.it>
2017-03-15 18:02:53 +0200

    final fix

---
+++ pyKRR.py
@@ -0,0 +1,69 @@
+import numpy
+
+
+
+
+class KRRsolver:
+    
+    
+    def __init__(self):
+        
+        self.kmatrix = None
+        self.Dmatrix = None
+        
+        self.alpha = 0.5
+        
+        self.nTrain = 0
+        self.trainIn = None
+        self.trainOut = None
+        
+        print "KRR solver initialised."
+        
+    
+    
+    def Train(self, trainIn, trainOut):
+        
+        self.nTrain = trainIn.shape[0]
+        self.trainIn = trainIn
+        self.trainOut = trainOut
+        
+        # start with a 0 matrix of the right size
+        self.kmatrix = numpy.zeros((self.nTrain,self.nTrain))
+
+        for i in xrange(self.nTrain):
+            for j in xrange(0,i+1):
+
+                # compute the distance between molecule i and j
+                self.kmatrix[i,j] = numpy.linalg.norm(trainIn[i]-trainIn[j])
+
+                # it is the same between j and i!
+                self.kmatrix[j,i] = self.kmatrix[i,j]
+
+            # add the -alpha on the diagonal
+            self.kmatrix[i,i] -= self.alpha
+
+        # invert the matrix
+        self.kmatrix = numpy.linalg.inv(self.kmatrix)
+    
+    
+    def Evaluate(self, validM):
+        
+        nValid = validM.shape[0]
+        
+        self.Dmatrix = numpy.zeros((nValid, self.nTrain))
+        for i in xrange(nValid):
+            for j in xrange(self.nTrain):
+                self.Dmatrix[i,j] = numpy.linalg.norm(validM[i]-self.trainIn[j])
+        
+        Epredict = numpy.dot(self.trainOut, numpy.dot(self.kmatrix, numpy.transpose(self.Dmatrix)))
+        
+        return Epredict
+    
+    
+    
+                
+                
+                
+                
+                
+                
